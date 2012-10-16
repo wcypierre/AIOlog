776907921f59e565da8b76feab859594481b38d4
wcypierre <wcypierre@gmail.com>
2012-10-16 22:41:13 +0800

    added gpl v3 license and added a parameter to the kmsg logging for it to stop automatically

--- log.cpp
+++ log.cpp
@@ -1,3 +1,23 @@
+/* 
+log.cpp
+This file contains all the codes to log information from an android device
+
+Copyright (c) 2012, wcypierre <wcypierre@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
@@ -134,12 +154,12 @@ void log_kmsg()
     if(separator_status == 0)
     {
         strcpy(command, separator);
-        strcat(command, "adb shell cat /proc/kmsg > kmsg.txt");
+        strcat(command, "adb shell cat -f /proc/kmsg > kmsg.txt");
     }
     else if(separator_status == 1)
     {
         strcpy(command, separator);
-        strcat(command, "adb shell cat /proc/kmsg > kmsg.txt");
+        strcat(command, "adb shell cat -f /proc/kmsg > kmsg.txt");
     }
 
     system(command);
